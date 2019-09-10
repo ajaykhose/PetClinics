91e191ca53f5fc556e96b7db1e265b80b725069d
Dhananjay Khose <ajaykhose@gmail.com>
2019-09-09 20:30:45 -0400

    quit
    initial commey

---
+++ src/main/java/com/springpetclinic/petclinics/PetclinicsApplication.java
@@ -0,0 +1,16 @@
+package com.springpetclinic.petclinics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicsApplication {
+
+    public static void main(String[] args) {
+
+                SpringApplication.run(PetclinicsApplication.class, args
+        //System.out.println("This is Test");
+        );
+    }
+
+}
---
+++ src/test/java/com/springpetclinic/petclinics/PetclinicsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springpetclinic.petclinics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetclinicsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
